1023186230f54ab03cbf92148871b77f27224279
Ayush Kumar <40270440+kumarayush2104@users.noreply.github.com>
2022-09-26 14:41:14 +0530

    Java: Program 13

---
+++ Programming with Java/Program13.java
@@ -0,0 +1,31 @@
+// Ayush Kumar
+// 202103103510253 (20)
+// CS-1
+
+import java.util.Scanner;
+
+class MyException extends Exception {
+    public MyException(String ex) {
+        super(ex);
+    }
+}
+
+class Program13 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Dividend: ");
+        float num1 = scan.nextFloat();
+        System.out.print("Enter Divisor: ");
+        float num2 = scan.nextFloat();
+        
+        try {
+            if(num2 == 0) {
+                throw new MyException("Division by 0 is not supported");
+            } else {
+                System.out.println("Answer: " + (num1/num2));
+            }
+        } catch(Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
