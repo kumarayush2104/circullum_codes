982ffc6929009368eb78e8dd2074acde21801012
Ayush Kumar <40270440+kumarayush2104@users.noreply.github.com>
2022-10-06 23:18:06 +0530

    Web Development: Practical 8: Minor changes

--- Web Development/Practical8/script.php
+++ Web Development/Practical8/script.php
@@ -2,11 +2,10 @@
     $mime = ['application/pdf', 'image/png', 'application/x-php'];
     if(in_array($_FILES['myFile']['type'], $mime) && $_FILES['myFile']['size'] <= 400*1024) {
         move_uploaded_file($_FILES['myFile']['tmp_name'], "uploads/".$_FILES['myFile']['name']);
-        foreach($_FILES['myFile'] as $key => $value) {
-            echo $key.': '.$value."<br>";
-        }
-        echo "<br><a href='uploads/".$_FILES['myFile']['name']."'><button>Download</button><br>";
+        header('Content-Disposition: attachment; filename="'.$_FILES['myFile']['name'].'"');
+        readfile("uploads/".$_FILES['myFile']['name']);
     } else {
-        echo "Check your file and try again<br><br>";
+        echo "Check your file and try again<br>";
+        echo "<br><a href='index.php'><Button>Back</Button></a><br>";
     }
 ?>
